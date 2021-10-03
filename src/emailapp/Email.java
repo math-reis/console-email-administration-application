8e202c0f6c6ac6e0ef81d25e237fca2f290ea275
Matheus Reis <math.reis.dev@gmail.com>
2021-10-03 08:58:21 -0300

    setDepartment

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,20 +1,38 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
-    private String passworkd;
+    private String password;
     private String department;
     private String alternateEmail;
     private int mailboxCapacity;
 
-    // Constructure to receive first and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-    // Ask for department
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 
+        this.department = setDepartment();
+    }
+
+    // Ask for department
+    private String setDepartment() {
+        System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "Sales";
+        } else if (depChoice == 2) {
+            return "Development";
+        } else if (depChoice == 3) {
+            return "Accounting";
+        } else {
+            return "";
+        }
+    }
     // Generate random password
 
     // Set emailbox capacity
